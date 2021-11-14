d7ab6c1038ca7d6ad7c8baf0ab5ccd8130a88c6b
harryCreatives4u <harryCreatives4u@gmailcom>
2021-11-14 14:05:08 +0530

    making the app ready to deploy

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBuen19hXaq5P9qoYfGzRw6mxywjNyMV5s",
+  authDomain: "talktack-dev.firebaseapp.com",
+  projectId: "talktack-dev",
+  storageBucket: "talktack-dev.appspot.com",
+  messagingSenderId: "608790362841",
+  appId: "1:608790362841:web:1d76a041b780680acf9cc8",
+};
+
+export const apiClientId =
+  "251030309310-qfbn4e1mva3didfc0s1ick3lbsumadkr.apps.googleusercontent.com";
+
+export const databaseUrl = "https://talktack-dev-default-rtdb.firebaseio.com";
