730a3a76b8282063e40db640ca0521092583beb0
harryCreatives4u <92977806+harryCreatives4u@users.noreply.github.com>
2021-11-14 15:51:06 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -8,6 +8,7 @@ export const firebaseConfig = {
 };
 
 export const apiClientId =
-  "251030309310-qfbn4e1mva3didfc0s1ick3lbsumadkr.apps.googleusercontent.com";
+     "608790362841-buh41uobs986q9jsio3cgm6ctuvrjgu9.apps.googleusercontent.com";
+//   "251030309310-qfbn4e1mva3didfc0s1ick3lbsumadkr.apps.googleusercontent.com";
 
 export const databaseUrl = "https://talktack-dev-default-rtdb.firebaseio.com";
