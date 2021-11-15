f1a11d69d402523deccc8a76c8b068b1bb4663b1
harryCreatives4u <92977806+harryCreatives4u@users.noreply.github.com>
2021-11-15 14:58:30 +0530

    removed sensitive information

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,14 +1,7 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBuen19hXaq5P9qoYfGzRw6mxywjNyMV5s",
-  authDomain: "talktack-dev.firebaseapp.com",
-  projectId: "talktack-dev",
-  storageBucket: "talktack-dev.appspot.com",
-  messagingSenderId: "608790362841",
-  appId: "1:608790362841:web:1d76a041b780680acf9cc8",
+  ...firebase config
 };
 
-export const apiClientId =
-     "608790362841-buh41uobs986q9jsio3cgm6ctuvrjgu9.apps.googleusercontent.com";
-//   "251030309310-qfbn4e1mva3didfc0s1ick3lbsumadkr.apps.googleusercontent.com";
+export const apiClientId ="google api id";
 
 export const databaseUrl = "https://talktack-dev-default-rtdb.firebaseio.com";
